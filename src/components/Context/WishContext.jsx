853846ba54b4c6e32b54414e5121d8563837346b
netojaycee <117773169+netojaycee@users.noreply.github.com>
2024-03-26 22:10:02 +0100

    wishset

--- src/components/Context/WishContext.jsx
+++ src/components/Context/WishContext.jsx
@@ -1,103 +1,151 @@
-import React, { createContext, useEffect, useState } from "react";
+import React, { createContext, useContext, useEffect, useState } from "react";
 import axios from "axios";
 import { apiUrl } from "../../config/env";
 import { useLoading } from "./LoadingContext";
+import AuthContext from "./AuthContext";
+
+const storedWish = JSON.parse(localStorage.getItem("wishEkomas")) || [];
 
 export const WishContext = createContext();
 
 const WishProvider = ({ children }) => {
-  const [wish, setWish] = useState([]);
- const { setIsLoading } = useLoading(true);
+  const [wish, setWish] = useState(storedWish);
+  const { setIsLoading } = useLoading(true);
+  const [pendingChanges, setPendingChanges] = useState(false);
+  const [isWish, setIsWish] = useState(false);
+  const { auth } = useContext(AuthContext);
+
   useEffect(() => {
     const fetchWishItems = async () => {
       try {
-        let token = localStorage.getItem("user");
-        token = token.replace(/['"]+/g, "");
-        // console.log(token)
+        // Check if the user is logged in
+        if (auth?.user) {
+          let token = localStorage.getItem("user");
+          token = token.replace(/['"]+/g, "");
 
-        const headers = {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${token}`,
-        };
+          const headers = {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          };
 
-        const response = await axios.get(`${apiUrl}/product/user-fav`, {
-          headers,
-        }); // Replace with your API endpoint
+          const response = await axios.get(`${apiUrl}/product/user-fav`, {
+            headers,
+          }); // Replace with your API endpoint
 
-        console.log(response.data.favorites);
-        localStorage.setItem(
-          "wishlist",
-          JSON.stringify(response.data.favorites)
-        );
-        const wishlist = localStorage.getItem("wishlist");
-        setWish(JSON.parse(wishlist));
+          console.log("initial", response.data.favorites);
+          localStorage.setItem(
+            "wishEkomas",
+            JSON.stringify(response.data.favorites)
+          ); // Save cart data to local storage
 
-        setIsLoading(false);
+          setWish(response.data.favorites);
+
+          setIsLoading(false);
+        }
       } catch (error) {
         console.error("Error fetching wish items:", error);
       }
     };
 
     fetchWishItems();
-  }, []); // Empty dependency array ensures thifect runs only once on component mount
+  }, [auth?.user]); // Include auth.user in the dependency array
 
-  const addToWish = async (productId) => {
-    try {
-      setIsLoading(true)
-      let token = localStorage.getItem("user");
-      token = token.replace(/['"]+/g, "");
-      const headers = {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${token}`,
-      };
+  useEffect(() => {
+    localStorage.setItem("wishEkomas", JSON.stringify(wish)); // Save cart data to local storage
+  }, [wish]);
 
-      const response = await axios.post(
-        `${apiUrl}/product/add-to-fav`,
-        { favourite: productId },
-        { headers }
-      );
+  const addToWish = (data, _id) => {
+    const newItem = { ...data };
+    const wishItemIndex = wish.findIndex((item) => item._id === _id);
 
-      localStorage.setItem("wishlist", JSON.stringify(response.data.favorites));
-      const wishlist = localStorage.getItem("wishlist");
-      setWish(JSON.parse(wishlist));
-      setIsLoading(false)
-    } catch (error) {
-      console.error("Error adding to wish list:", error);
+    if (wishItemIndex !== -1) {
+      // If item already exists, remove it
+      const newWish = [...wish];
+      newWish.splice(wishItemIndex, 1);
+
+      setWish(newWish);
+      setIsWish(false);
+      setPendingChanges(true);
+    } else {
+      // If item does not exist, add it
+
+      const updatedWish = [...wish, newItem]; // Add the new item to the wishlist
+
+      setWish(updatedWish);
+
+      setIsWish(true);
+      setPendingChanges(true);
     }
   };
 
-  const removeFromWish = async (productId) => {
-    try {
-      setIsLoading(true)
-      let token = localStorage.getItem("user");
-      token = token.replace(/['"]+/g, "");
-      const headers = {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${token}`,
-      };
+  const removeFromWish = (_id) => {
+    const newWish = wish.filter((item) => item._id !== _id);
 
-      const response = await axios.post(
-        `${apiUrl}/product/remove-from-fav`,
-        { productId },
-        { headers }
-      );
-      // Replace with your API endpoint
-      localStorage.setItem("wishlist", JSON.stringify(response.data.favorites));
-      const wishlist = localStorage.getItem("wishlist");
-      setWish(JSON.parse(wishlist));
-      setIsLoading(false)
+    setWish(newWish);
+    setIsWish(false);
+    setPendingChanges(true);
+  };
+
+  useEffect(() => {
+    // Synchronize with backend after a certain delay or on pending changes
+    if (auth?.user) {
+      const timer = setTimeout(() => {
+        if (pendingChanges) {
+          syncWithBackend();
+          setPendingChanges(false); // Reset pending changes flag after synchronization
+        }
+      }, 2000); // Example: Synchronize every 2 seconds or adjust as needed
+
+      return () => clearTimeout(timer); // Cleanup timer on component unmount
+    }
+  }, [auth?.user, wish, pendingChanges]);
+
+  const syncWithBackend = async () => {
+    try {
+      // Check if wish array is empty
+      if (wish.length === 0) {
+        // If wish array is empty, send an empty array to the backend
+        const token = localStorage.getItem("user").replace(/['"]+/g, "");
+        const headers = {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        };
+
+        const response = await axios.post(
+          `${apiUrl}/product/add-to-fav`,
+          { favourites: [] },
+          { headers }
+        );
+
+        setWish(response.data.favorites);
+      } else {
+        // If wish array is not empty, map the wish array to extract IDs
+        const wishIds = wish.map((item) => item._id);
+
+        const token = localStorage.getItem("user").replace(/['"]+/g, "");
+        const headers = {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        };
+
+        const response = await axios.post(
+          `${apiUrl}/product/add-to-fav`,
+          { favourites: wishIds },
+          { headers }
+        );
+
+        setWish(response.data.favorites);
+      }
+
+      setIsLoading(false);
     } catch (error) {
-      console.error("Error removing from wish list:", error);
+      console.error("Error synchronizing wishlist with backend:", error);
+      setIsLoading(false);
     }
   };
 
-  const clearWish = async () => {
-    try {
-      const response = await axios.post("/api/wishlist/clear"); // Replace with your API endpoint
-      setWish(response.data.wishItems);
-    } catch (error) {
-      console.error("Error clearing wish list:", error);
-    }
+  const clearWish = () => {
+    setWish([]);
   };
 
   return (
@@ -107,6 +155,8 @@ const WishProvider = ({ children }) => {
         addToWish,
         removeFromWish,
         clearWish,
+        setIsWish,
+        isWish,
       }}
     >
       {children}
@@ -115,81 +165,3 @@ const WishProvider = ({ children }) => {
 };
 
 export default WishProvider;
-
-// import React, { createContext, useEffect, useState } from "react";
-// import axios from "axios";
-// import { apiUrl } from "../../config/env";
-
-// export const WishContext = createContext();
-
-// const WishProvider = ({ children }) => {
-//   const [wish, setWish] = useState([]);
-//   const [loading, setLoading] = useState(true);
-
-//   useEffect(() => {
-//     const fetchWishItems = async () => {
-//       try {
-//         let token = localStorage.getItem("user");
-//         token = token.replace(/['"]+/g, "");
-
-//         const headers = {
-//           "Content-Type": "application/json",
-//           Authorization: `Bearer ${token}`,
-//         };
-
-//         const response = await axios.get(`${apiUrl}/product/user-fav`, {
-//           headers,
-//         });
-//         localStorage.setItem(
-//           JSON.stringify(("wishlist", response.data.response))
-//         );
-//         const wishlist = localStorage.getItem(JSON.parse("wishlist"));
-//         setWish(wishlist);
-//         setLoading(false);
-//       } catch (error) {
-//         console.error("Error fetching wish items:", error);
-//       }
-//     };
-
-//     fetchWishItems();
-//   }, []); // Empty dependency array ensures this effect runs only once on component mount
-
-//   const handleToggleFavorite = async (productId) => {
-//     try {
-//       let token = localStorage.getItem("user");
-//       token = token.replace(/['"]+/g, "");
-//       const headers = {
-//         "Content-Type": "application/json",
-//         Authorization: `Bearer ${token}`,
-//       };
-
-//       const response = await axios.post(
-//         `${apiUrl}/product/fav`,
-//         { productId },
-//         { headers }
-//       );
-
-//       localStorage.setItem(
-//         JSON.stringify(("wishlist", response.data.response))
-//       );
-//       const wishlist = localStorage.getItem(JSON.parse("wishlist"));
-//       setWish(wishlist);
-//     } catch (error) {
-//       console.error("Error toggling favorite:", error);
-//     }
-//   };
-
-//   return (
-//     <WishContext.Provider
-//       value={{
-//         wish,
-//         handleToggleFavorite,
-//         loading,
-//       }}
-//     >
-//       {children}
-//     </WishContext.Provider>
-//   );
-// };
-
-// export default WishProvider;
--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -5,8 +5,9 @@ import { HeartIcon } from "@heroicons/react/24/outline";
 import Aos from "aos";
 import { WishContext } from "./Context/WishContext";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHeart } from "@fortawesome/free-solid-svg-icons";
+import { faHeart, faTrash } from "@fortawesome/free-solid-svg-icons";
 import { faHeart as faHeartOutline } from "@fortawesome/free-regular-svg-icons";
+import { toast } from "react-toastify";
 
 const ProductItem = ({
   _id,
@@ -19,22 +20,32 @@ const ProductItem = ({
 }) => {
   const productDetailPath = `/product/${_id}`;
 
-  const { addToWish, removeFromWish, wish } = useContext(WishContext);
-  const [isInWishList, setIsInWishList] = useState(false);
+  const { addToWish, removeFromWish, wish, clearWish } = useContext(WishContext);
 
   useEffect(() => {
     Aos.init();
   }, []);
 
-  useEffect(() => {
-    setIsInWishList(wish && wish.some((item) => item._id === _id));
-  }, [wish, _id]);
+  const isWish = wish.some((item) => item._id === _id);
 
-  const handleWishClick = () => {
-    if (isInWishList) {
-      removeFromWish(_id);
+  const toggleWish = () => {
+    if (!isWish) {
+      addToWish(
+        {
+          _id,
+          name,
+          price,
+          image,
+          discount,
+          classx,
+          category,
+        },
+        _id
+      );
+      toast.success(`${name} added to wishlist`);
     } else {
-      addToWish(_id);
+      removeFromWish(_id);
+      toast.success(`${name} removed from wishlist`);
     }
   };
 
@@ -45,15 +56,6 @@ const ProductItem = ({
     >
       <div className="flex flex-row gap-4 rounded ">
         <div className="bg-white flex flex-row p-2 rounded overflow-hidden pr-0 relative w-full h-[200px] duration-300 transform hover:scale-105 transition ease-linear">
-          <div className="top-4 left-3 absolute">
-            <FontAwesomeIcon
-              icon={isInWishList ? faHeart : faHeartOutline}
-              style={{ color: isInWishList ? "red" : "gray" }}
-              size="lg"
-              className="cursor-pointer"
-              onClick={handleWishClick}
-            />
-          </div>
           <div
             className="group flex justify-center items-center h-32 w-full bg-white px-4 py-2"
             data-aos="zoom-out"
@@ -66,10 +68,15 @@ const ProductItem = ({
                 style={{ maxHeight: "100%" }}
               />
             </Link>
-            <a href={productDetailPath} className="hidden group-hover:block text-xs absolute bottom-0 left-0 right-0 py-1 px-8 text-center bg-red-500 text-white border-b-2 border-secondary">Shop Now</a>
+            <a
+              href={productDetailPath}
+              className="hidden group-hover:block text-xs absolute bottom-0 left-0 right-0 py-1 px-8 text-center bg-red-500 text-white border-b-2 border-secondary"
+            >
+              Shop Now
+            </a>
             <a
               href="#"
-              className="hidden group-hover:block text-xs text-base absolute bottom-0 left-0 right-0 py-1 px-8 text-center bg-gray-300 text-red-900 border-b-2 border-secondary"
+              className="hidden group-hover:block text-base absolute bottom-0 left-0 right-0 py-1 px-8 text-center bg-gray-300 text-red-900 border-b-2 border-secondary"
             >
               Shop Now
             </a>
@@ -87,12 +94,23 @@ const ProductItem = ({
           </div>
         </div>
       </div>
-
-      <div className="flex flex-col px-4 gap-1" data-aos="zoom-in">
-        <div className="mt-1 text-[#b32b2b] text-sm lg-text-lg md:text-md">
-          {name}
+      <div className="flex flex-row gap-4 rounded ">
+        <div className="flex flex-col px-4 gap-1" data-aos="zoom-in">
+          <div className="mt-1 text-[#b32b2b] text-sm lg-text-lg md:text-md">
+            {name}
+          </div>
+          <div className="text-gray-600 sm:text-sm md:text-lg">${price}</div>
         </div>
-        <div className="text-gray-600 sm:text-sm md:text-lg">${price}</div>
+        <div className="">
+          <FontAwesomeIcon
+            icon={isWish ? faHeart : faHeartOutline}
+            style={{ color: isWish ? "red" : "gray" }}
+            size="lg"
+            className="cursor-pointer"
+            onClick={toggleWish}
+          />
+          <FontAwesomeIcon icon={faTrash} onclick={clearWish}/>
+        </div>
       </div>
     </div>
   );
