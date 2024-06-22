c6a435e6509d68161042224223ef857434b5b69a
John Chinonso Edeh <netojaycee@gmail.com>
2024-06-22 21:47:19 +0100

    k

--- src/config/env.js
+++ src/config/env.js
@@ -3,3 +3,7 @@
 // export const apiUrl = "https://ekomas.miladyfans.com";
 export const apiUrl = "https://commerce-vujf.onrender.com";
 // export const apiUrl = "http://localhost:5004";
+
+
+// centralize the topic, remove reg bg, use reed text, put view more under product
+// justrite.com, shoprite.com
