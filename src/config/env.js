7f9713b4a0e3a3180548c70be71d7bfa1e1af3f8
John Chinonso Edeh <netojaycee@gmail.com>
2024-09-05 02:58:22 +0100

    up

--- src/config/env.js
+++ src/config/env.js
@@ -1,5 +1,5 @@
 // export const apiUrl = "https://commerce-vujf.onrender.com";
-export const apiUrl = "https://yoamart.com/api/";
+export const apiUrl = "https://yoamart.com/api";
 // export const apiUrl = "http://localhost:5004";
 
 // centralize the topic, remove reg bg, use reed text, put view more under product
